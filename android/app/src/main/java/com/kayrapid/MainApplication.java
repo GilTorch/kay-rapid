68a0d87a1c4082d130b9f6852b837702a972bc9d
GilTorch <infotorch2014@gmail.com>
2019-07-17 19:40:42 -0400

    react-native-image-picker successfully added

--- android/app/src/main/java/com/kayrapid/MainApplication.java
+++ android/app/src/main/java/com/kayrapid/MainApplication.java
@@ -3,6 +3,7 @@ package com.kayrapid;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
             new FBSDKPackage(mCallbackManager)
       );
